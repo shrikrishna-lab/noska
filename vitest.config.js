c23d01622b038d65697ee294412b5cddfc617fb2
Not-krrish <krishnahandibag77@gmail.com>
2026-07-27 19:35:22 +0530

    fix: use threads pool for jsdom 30 compatibility

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    pool: "threads",
     globals: true,
     setupFiles: ["./src/test/setup.js"],
     exclude: ["**/node_modules/**", "**/dist/**", "**/e2e/**"],
